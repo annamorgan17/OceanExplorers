55681342c8c9153f7b0181e6c4ba9113f5d786ee
Anna Morgan <34129238+annamorgan17@users.noreply.github.com>
2021-02-08 20:13:28 +0000

    behaviour fish stuff added

---
+++ Anna's Fish Behaviour/Assets/scripts/crab/floorDetection.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class floorDetection : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+      
+    }
+    private void OnTriggerExit(Collider other)
+    {
+      
+    }
+}
