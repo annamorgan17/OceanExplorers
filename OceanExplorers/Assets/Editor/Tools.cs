2ea328328fb17e904cbb3e7521873d294a4ace32
PhoebePudge <55756132+PhoebePudge@users.noreply.github.com>
2021-02-09 12:58:58 +0000

    Lystem and shader implimentation

---
+++ OceanExplorers/Assets/Editor/Tools.cs
@@ -0,0 +1,290 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System;
+using System.Reflection;
+public class Tools{ 
+    [MenuItem("GameObject/CreatePlant")] private static void CreatePlant() {
+        if (EditorUtility.DisplayDialog("Plant Generator", "Do you want to create a plant?", "Create", "Cancel")) {
+            int Index = 1;
+            foreach (GameObject gameObj in GameObject.FindObjectsOfType<GameObject>()) {
+                if (gameObj.name.Contains("Plant")) 
+                    Index++; 
+            } 
+            GameObject gm = new GameObject("Plant " + Index);
+            gm.AddComponent<Generation>();
+        }
+    }  
+} 
+[CustomEditor(typeof(Generation))] public class Interface : Editor {
+    static Generation myScript;
+    int[] _choiceIndex = new int[10]; 
+    int _prevRotation = 0;
+    int PreviousRuleLength = 1;
+    int RuleLength = 1;
+    string[] _choices = new[] { 
+
+        //tree branching types
+        "F+[+F-F-F]-[--F+F+F]", 
+        "FF-[-F+F+F]+[+F-F-F]",
+        "FF-[<-F+F+F>]+[<+F-F-F>]",
+        "F[+FF][-FF]F[-F][+F]F",
+        "F[+F]F[-F][F]",
+         
+        //traingle
+        //120
+        //F+F+F
+        "F-F+F",
+
+        //square sierpinski
+        //90
+        //F+XF+F+XF
+        //X:
+        "XF-F+F-XF+F+XF-F+F-X",
+
+        //quadratic snowflake
+        //F
+        //90
+        "F-F+F+F-F", 
+
+        //board
+        //F+F+F+F
+        //90
+        "FF+F+F+F+FF",
+
+        //tiles
+        //start F+F+F+F
+        //90
+        "FF+F-F+F+FF",
+
+        //rings
+        //start F+F+F+F
+        // 90
+        "FF+F+F+F+F+F-F",
+
+        //levey
+        //start F
+        //45
+        "-F++F-",
+
+        //island curve
+        //Start F-F-F-F
+        //Angle 90
+        //Rule F
+        "F+f-FF+F+FF+Ff-f+FF-F-FF-Ff-FFF",
+        //Rule f
+        "ffffff",
+
+        //Use same settings as island curve but onle rule with Rule F
+        "F+FF-FF-F-F+F+FF-F-F+F+FF+FF-F",
+
+        //Wiliam McWorters Pentadendrite
+        //Start F-F-F-F-F
+        //Angle 72
+        //Iterations 4
+        "F-F-F++F+F-F",
+
+        //Dragon Curve
+        //Start FA
+        //Rule A
+        "A+BF+",
+        //Rule B
+        "-FA-B",
+
+        //Gosper Curve
+        //Angle 60
+        //Rule F
+        "F-G--G+F++FF+G-",
+        //Rule G
+        "+F-GG--G-F++F+G",
+
+        //Round Star
+        //Angle 77
+        //Iterations 7
+        "F++F",
+
+        //Ice fractal
+        //Angle 90
+        //Start F+F+F+F 
+        "FF+F++F+F",
+
+        //Koch curve
+        //angle 80
+        "F+F--F+F", 
+
+        //Koch Snowflake
+        //Angle 60
+        //Start F--F--F
+        "F+F--F+F",
+
+        //Hilburt Curve
+        //Angle 90
+        //Rule L
+        "+RF-LFL-FR+",
+        //Rule R
+        "-LF+RFR+FL-",
+
+        "F-[[X]+X]+F[+FX]-X",
+        "FF",
+
+        "F(L,G)+[+F(L,G)-F(L,G)-F(L,G)]-[--F(L,G)+F(L,G)+F(L,G)]",
+        "Custom String"}; // custom
+
+void OnEnable() {
+//EditorVariables stores data like choice index and previous rotation as this is lost after clicking on something else
+//asign target is there is nothing
+if (myScript == null) {
+    myScript = (Generation)target;
+}
+
+//load values
+_choiceIndex = myScript.EditorVariables._choiceIndex;
+_prevRotation = myScript.EditorVariables._prevRotation;
+
+//intiilise values if null
+if (_choiceIndex == null) {
+    _choiceIndex = new int[10];
+    _prevRotation = 0;
+}
+}
+private void OnDisable() {
+//set the values when focus is lost
+myScript.EditorVariables._choiceIndex = _choiceIndex;
+myScript.EditorVariables._prevRotation = _prevRotation;
+}
+public override void OnInspectorGUI() { 
+//Check if control was changed inside a block of code
+EditorGUI.BeginChangeCheck();
+
+#region Prefab Loading
+//Filepath
+string path = "Assets/Resources/Prefabs/";  
+string fileExstension = "prefab";
+//Button save
+if (GUILayout.Button("Save to prefab")) {
+    //save location
+    string filepath = EditorUtility.SaveFilePanel("Select location to save Prefab", path, "", fileExstension); 
+    //path exists
+    if (filepath.Length != 0) {
+        bool success;
+        //save
+        PrefabUtility.SaveAsPrefabAsset(myScript.gameObject, filepath, out success);
+
+        //debug success
+        if (success) {
+            Debug.Log("Success in saving file to: " + filepath);
+        } else {
+            Debug.Log("Failed in saving file to: " + filepath);
+        }
+    } 
+    //validate script
+    Validate(myScript);
+}
+//Button load
+if (GUILayout.Button("Load from prefab")) {
+    //Choose file
+    string filepath = EditorUtility.OpenFilePanel("Select Prefab to Open", path, fileExstension);
+    //File exists
+    if (filepath.Length != 0) {
+        //Load
+        GameObject gm = Instantiate<GameObject>(PrefabUtility.LoadPrefabContents(filepath));
+        Debug.Log("Asset was found and loaded!");
+        gm.transform.rotation = myScript.gameObject.transform.rotation;
+        gm.name = myScript.gameObject.name;
+        myScript.gameObject.AddComponent<Destroy>();
+    } else {
+        Debug.LogError("Failed loading prefab contents");
+    } 
+    //Validate script
+    Validate(myScript); 
+}
+#endregion 
+
+#region Colour 
+//Toggle solid
+EditorGUILayout.LabelField("Colour", EditorStyles.boldLabel);
+myScript.SolidColour = EditorGUILayout.Toggle("SolidColour", myScript.SolidColour);
+//Flip colour
+if (GUILayout.Button("FlipColour")) {
+    //set to opposite and revalidate
+    myScript.FlipColour = !myScript.FlipColour;
+    Validate(myScript);
+}
+//Solid Colour
+if (myScript.SolidColour) {
+    //display appropirate colour dependant on flip
+    if (myScript.FlipColour)
+        myScript.Col2 = EditorGUILayout.ColorField("Col2", myScript.Col2);
+    else
+        myScript.Col1 = EditorGUILayout.ColorField("Col1", myScript.Col1);
+} else {
+    //show colours in flipped order
+    if (myScript.FlipColour) {
+        myScript.Col2 = EditorGUILayout.ColorField("Col2", myScript.Col2);
+        myScript.Col1 = EditorGUILayout.ColorField("Col1", myScript.Col1);
+    } else {
+        myScript.Col1 = EditorGUILayout.ColorField("Col1", myScript.Col1);
+        myScript.Col2 = EditorGUILayout.ColorField("Col2", myScript.Col2);
+    }
+}
+#endregion
+
+#region Rotation
+//Display rotate toggle
+EditorGUILayout.LabelField("Rotation", EditorStyles.boldLabel);
+myScript.rotate = EditorGUILayout.Toggle("rotate", myScript.rotate);
+
+//rotation
+if (!myScript.rotate) {
+    EditorGUILayout.LabelField("Rotation Angle");
+    myScript.RotationAngle = EditorGUILayout.IntSlider(myScript.RotationAngle, 0, 360);
+}
+#endregion
+
+DrawDefaultInspector();
+
+#region Dictionary String  
+//Display rule length
+EditorGUILayout.LabelField("Rules", EditorStyles.boldLabel);
+myScript.RuleLength = EditorGUILayout.IntSlider(myScript.RuleLength, 1, 10);
+//For each rule
+for (int i = 0; i < myScript.RuleLength; i++) {
+    //Display Rule
+    EditorGUILayout.LabelField("Rule " + i);
+    string Rule = EditorGUILayout.TextField("Rule", myScript.dictionaryChar[i].ToString());
+    //Dont allow it to be blank
+    if (Rule == "")
+        Rule = "F";
+    myScript.dictionaryChar[i] = Rule.ToCharArray()[0];
+    //find index
+    _choiceIndex[i] = EditorGUILayout.Popup(_choiceIndex[i], _choices);
+    if (_choiceIndex[i] == _choices.Length - 1) {
+        //custom string
+        string String = EditorGUILayout.TextField("Rule", myScript.dictionaryString[i]);
+        //dont allow to be empty
+        if (String == "")
+            String = "F";
+        myScript.dictionaryString[i] = String;
+    } else {
+        //dropdown string option
+        myScript.dictionaryString[i] = _choices[_choiceIndex[i]];
+    }
+}
+#endregion
+
+
+//validate
+if (GUILayout.Button("Recalculate"))
+    Validate(myScript);
+//rotate
+if (GUI.changed) 
+    myScript.Rotation(); 
+_prevRotation = myScript.RotationAngle;
+
+}
+//Call validation in the script
+private void Validate(Generation myScript) { 
+if (Application.isPlaying) { myScript.Validate = true; }
+} 
+} 
---
+++ OceanExplorers/Assets/Shaders/Caustics.shader
@@ -0,0 +1,124 @@
+// Upgrade NOTE: replaced '_Object2World' with 'unity_ObjectToWorld'
+// Upgrade NOTE: replaced 'mul(UNITY_MATRIX_MVP,*)' with 'UnityObjectToClipPos(*)'
+
+Shader "Water/Caustics"
+{
+    Properties
+    {
+		_SurfaceNoise("Surface Noise", 2D) = "white" {} 
+		_SurfaceNoiseScroll("Surface Noise Scroll Amount", Vector) = (0.03, 0.03, 0, 0) 
+		_SurfaceNoiseCutoff("Surface Noise Cutoff", Range(0, 1)) = 0.777 
+
+		_SurfaceDistortion("Surface Distortion", 2D) = "white" {}	 
+		_SurfaceDistortionAmount("Surface Distortion Amount", Range(0, 1)) = 0.27 
+
+        _WaterHeight("Water Height", Range(0,20)) = 10
+        _Color ("Color", Color) = (1,1,1,1)
+        _MainTex ("Albedo (RGB)", 2D) = "white" {}
+        _Smoothness ("Smoothness", Range(0,1)) = 0.5  
+        _Metallic ("Metallic", Range(0,1)) = 0.0
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 200  
+        CGPROGRAM
+        #define SMOOTHSTEP_AA 0.01
+        // Physically based Standard lighting model, and enable shadows on all light types
+        #pragma surface surf Standard
+
+        // Use shader model 3.0 target, to get nicer looking lighting
+        #pragma target 3.0
+
+
+        struct Input
+        {
+            float2 uv_MainTex; 
+            float3 worldPos;
+        };
+
+        sampler2D _SurfaceNoise;
+        float4 _SurfaceNoise_ST; 
+ 
+ 		sampler2D _SurfaceDistortion;
+		float4 _SurfaceDistortion_ST;
+    	
+        float _SurfaceNoiseCutoff;
+		float _SurfaceDistortionAmount;
+        float2 _SurfaceNoiseScroll;
+        fixed4 _WaterTint;
+
+        sampler2D _MainTex; 
+        
+        half _Smoothness;
+        half _Specular;
+        half _Metallic;
+        fixed4 _Color;
+        fixed3 caustics;
+        float _WaterHeight;
+
+        sampler2D _CameraDepthTexture;
+		sampler2D _CameraNormalsTexture;
+
+        UNITY_INSTANCING_BUFFER_START(Props) 
+        UNITY_INSTANCING_BUFFER_END(Props)
+
+        void surf (Input IN, inout SurfaceOutputStandard o)
+        {
+            float3 localPos = IN.worldPos;
+
+            // Albedo comes from a texture tinted by color
+            
+            fixed4 c = tex2D (_MainTex, IN.uv_MainTex) * _Color;
+            o.Albedo = c.rgb;
+        
+            // Caustics sampling
+            fixed2 uv = IN.uv_MainTex * _SurfaceNoise_ST.xy + _SurfaceNoise_ST.zw;
+
+            if (localPos.y < _WaterHeight) {   
+
+                float DistanceFromWater = localPos.y / 50;
+                float InvertedDistanceFromWater = -DistanceFromWater;
+                float2 distortSample = (tex2D(_SurfaceDistortion, uv).xy * 2 - 1) * (_SurfaceDistortionAmount * DistanceFromWater); 
+                float2 noiseUV = float2((uv.x + _Time.y * _SurfaceNoiseScroll.x) + distortSample.x,  (uv.y + _Time.y * _SurfaceNoiseScroll.y) + distortSample.y); 
+                float surfaceNoise = smoothstep(_SurfaceNoiseCutoff - SMOOTHSTEP_AA, _SurfaceNoiseCutoff + SMOOTHSTEP_AA, tex2D(_SurfaceNoise, noiseUV).r);  
+                float3 tone = tex2D(_SurfaceNoise, noiseUV).rgb; 
+                tone * surfaceNoise;
+                o.Albedo += (tone * InvertedDistanceFromWater);   
+            } else {
+                caustics = tex2D(_MainTex, uv).rgb;
+            }
+             
+            o.Metallic = _Metallic;
+            o.Smoothness = _Smoothness;
+            o.Alpha = c.a;
+        }
+        ENDCG
+        Pass {
+            Tags {"LightMode"="ShadowCaster"}
+
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            #pragma multi_compile_shadowcaster
+            #include "UnityCG.cginc"
+
+            struct v2f { 
+                V2F_SHADOW_CASTER;
+            };
+
+            v2f vert(appdata_base v)
+            {
+                v2f o; 
+                TRANSFER_SHADOW_CASTER_NORMALOFFSET(o)
+                return o;
+            }
+
+            float4 frag(v2f i) : SV_Target
+            {
+                SHADOW_CASTER_FRAGMENT(i)
+            }
+            ENDCG
+        }
+    }
+}
