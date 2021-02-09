61ba08661ec5fea1a0da5b836399044858b02abc
PhoebePudge <55756132+PhoebePudge@users.noreply.github.com>
2021-02-09 17:59:58 +0000

    Phoebe - Basic tile system

---
+++ OceanExplorers/Assets/Scripts/Terrain/TerainGen.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerainGen : MonoBehaviour
+{
+    [SerializeField] GameObject[] FloorTiles;
+    [SerializeField] GameObject Surface;
+    [SerializeField] int width;
+    [SerializeField] int height; 
+    [SerializeField] string seed;
+    [SerializeField] bool useRandomSeed = true; 
+    [Range(0, 100)] public int randomFillPercent; 
+    [SerializeField] int[,] map;
+    [SerializeField] int Size = 10;
+    private GameObject parent;
+
+    void Start(){
+        //Replace for less intensive search later
+        FloorTiles = Resources.LoadAll<GameObject>("Tiles/Floor");
+        Surface = Resources.Load<GameObject>("Tiles/Surface");
+        parent = new GameObject("Terrain");
+        GenerateMap();
+        
+    } 
+    void Update(){
+        if (Input.GetMouseButtonDown(0)) {
+            foreach (GameObject item in parent.transform) {
+                Destroy(item);
+            }
+            GenerateMap();
+        }
+    } 
+    void Draw() {
+        if (map != null) {
+            for (int x = 0; x < width; x++) {
+                for (int y = 0; y < height; y++) {
+                    Vector3 pos = new Vector3(-width / 2 + x + .5f, 0, -height / 2 + y + .5f);
+                    pos *= Size;
+                    if ((map[x, y] != 1)) {
+                        ////water tile
+                        GameObject floor = GameObject.Instantiate(FloorTiles[0]); 
+                        floor.transform.position = pos;
+                        floor.transform.SetParent(parent.transform);
+                    } else {
+                        GameObject surface = GameObject.Instantiate(Surface);
+                        pos.y += (surface.transform.localScale.y / 2);
+                        surface.transform.position = pos;
+                        surface.transform.SetParent(parent.transform);
+                    }
+                    //Gizmos.color = (map[x, y] == 1) ? Color.black : Color.white; 
+                    //Gizmos.DrawCube(pos, Vector3.one);
+                }
+            }
+        }
+    } 
+    void GenerateMap(){ 
+        map = new int[width, height];
+        RandomFillMap();
+        for (int i = 0; i < 5; i++) 
+            SmoothMap();
+        Draw();
+    } 
+    void RandomFillMap() {
+        if (useRandomSeed)
+            seed = Time.time.ToString();
+        System.Random pseudoRandom = new System.Random(seed.GetHashCode()); 
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                if (x == 0 || x == width - 1 || y == 0 || y == height - 1)
+                    map[x, y] = 1;
+                else 
+                    map[x, y] = (pseudoRandom.Next(0, 100) < randomFillPercent) ? 1 : 0;
+            }
+        }
+    } 
+    void SmoothMap() {
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                int neighbourWallTiles = GetSurroundingWallCount(x, y);
+                if (neighbourWallTiles > 4)
+                    map[x, y] = 1;
+                else if (neighbourWallTiles < 4)
+                    map[x, y] = 0;
+            }
+        }
+    } 
+    int GetSurroundingWallCount(int gridX, int gridY) {
+        int wallCount = 0;
+        for (int neighbourX = gridX - 1; neighbourX <= gridX + 1; neighbourX++) {
+            for (int neighbourY = gridY - 1; neighbourY <= gridY + 1; neighbourY++) {
+                if (neighbourX >= 0 && neighbourX < width && neighbourY >= 0 && neighbourY < height) {
+                    if (neighbourX != gridX || neighbourY != gridY) 
+                        wallCount += map[neighbourX, neighbourY];
+                } else 
+                    wallCount++;
+            }
+        } 
+        return wallCount;
+    }
+}
