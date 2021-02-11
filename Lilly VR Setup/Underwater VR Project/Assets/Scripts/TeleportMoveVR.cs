e82573de42c9d16c2c92863a387e970556496f33
lillianabunn <55756023+lillianabunn@users.noreply.github.com>
2021-02-11 10:31:37 +0000

    Fifth

---
+++ Lilly VR Setup/Underwater VR Project/Assets/Scripts/TeleportMoveVR.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VR;
+
+public class TeleportMoveVR : MonoBehaviour
+{
+    private void Update()
+    {
+        UnityEngine.Input.GetJoystickNames();
+    }
+}
