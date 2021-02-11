7801179e6b213a9767333fcce6df07c58e30e5fb
lillianabunn <55756023+lillianabunn@users.noreply.github.com>
2021-02-11 10:24:55 +0000

    Basic Set Up
    Attempted some sort of settings menu, working on teleport script

---
+++ Lilly VR Setup/Underwater VR Project/Assets/Scripts/ButtonChange.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonChange : MonoBehaviour
+{
+    [SerializeField] string sceneName;
+    [SerializeField] string buttonName;
+    void Update()
+    {
+        Debug.Log("Pressed button: " + buttonName);
+        SceneManager.LoadScene(sceneName);
+    }
+}
