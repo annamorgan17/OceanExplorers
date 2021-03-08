3b1c366687852d8c770656771ea9c59e7df430b9
Anna Morgan <34129238+annamorgan17@users.noreply.github.com>
2021-03-08 15:17:06 +0000

    crabs climb slopes
    still need to rotate up direction

--- OceanExplorers/Assets/Scripts/Behaviour/CrabData.cs
+++ OceanExplorers/Assets/Scripts/Behaviour/CrabData.cs
@@ -4,9 +4,6 @@ using UnityEngine;
 [CreateAssetMenu()]
 public class CrabData : ScriptableObject  {
     public Vector3 gameArea = Vector3.zero;
-    //[SerializeField] [Range(0.1f, 5.0f)] float crabSpeed;
-    //[SerializeField] [Range(1.0f, 5.0f)] float crabRotSpeed; 
-    //int layerMask = 1 << 8;
     public float randomAmount = 10000;
-    public int rayLength = 6; 
+
 }
--- OceanExplorers/Assets/Scripts/Behaviour/CrabMovement.cs
+++ OceanExplorers/Assets/Scripts/Behaviour/CrabMovement.cs
@@ -6,46 +6,32 @@ using UnityEngine.AI;
 public class CrabMovement : MonoBehaviour {
     public CrabData data;
     private Vector3 newPos = Vector3.zero;
-    private Quaternion targetRotation;
-    private Vector3 direction; 
 
     private void Start() {
         if (GetComponent<NavMeshAgent>() == null) { // adding a navmesh if needed
             gameObject.AddComponent<NavMeshAgent>();
         } 
-        targetRotation = Quaternion.LookRotation(newPos - transform.position);
-        TargetPosInstant(); 
+        TargetPosInstant();
     }
     private void Update() {
-        direction = (newPos - transform.position).normalized;
-        Debug.DrawRay(transform.position, direction * data.rayLength, Color.red);
         GetComponent<NavMeshAgent>().destination = newPos;
         TargetPos();
-
-        //if (!Physics.Raycast(this.transform.position, direction, rayLength, layerMask)) {
-        //    Debug.DrawRay(transform.position, direction * rayLength, Color.red); 
-        //    transform.position = Vector3.MoveTowards(this.transform.position, newPos, crabSpeed * Time.deltaTime);
-        //    transform.rotation = Quaternion.Lerp(this.transform.rotation, Quaternion.LookRotation(direction), crabRotSpeed * Time.deltaTime);
-        //    TargetPos();
-        //} else { 
-        //    transform.position = Vector3.MoveTowards(this.transform.position, direction * 1, crabSpeed * Time.deltaTime);
-        //    transform.rotation = Quaternion.Lerp(this.transform.rotation, Quaternion.LookRotation(direction * 1), crabRotSpeed * Time.deltaTime);
-        //} 
+ 
     }
 
     private void TargetPos() {
         if (Random.Range(0, data.randomAmount) < 50) {
             newPos = new Vector3(Random.Range(-data.gameArea.x, data.gameArea.x),
-                                 gameObject.transform.position.y,
+                                 Random.Range(-data.gameArea.y, data.gameArea.y),
                                  Random.Range(-data.gameArea.z, data.gameArea.z)); 
-            targetRotation = Quaternion.LookRotation(newPos - transform.position); 
+ 
         }
     }
 
     private void TargetPosInstant() {
         newPos = new Vector3(Random.Range(-data.gameArea.x, data.gameArea.x),
-                             gameObject.transform.position.y,
+                             Random.Range(-data.gameArea.y, data.gameArea.y),
                              Random.Range(-data.gameArea.z, data.gameArea.z)); 
-        targetRotation = Quaternion.LookRotation(newPos - transform.position); 
+
     }
 }
