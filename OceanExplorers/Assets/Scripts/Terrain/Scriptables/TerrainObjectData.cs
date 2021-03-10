885787b3e60ea6aea179afc84ce96fc9eeeb7349
PhoebePudge <55756132+PhoebePudge@users.noreply.github.com>
2021-03-10 16:49:04 +0000

    Phoebe - Terrain Object Spawning

---
+++ OceanExplorers/Assets/Scripts/Terrain/Scriptables/TerrainObjectData.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu()]
+public class TerrainObjectData : ScriptableObject {
+    [SerializeField] private TerrainObject[] terrainList = new TerrainObject[1];
+    [HideInInspector] public TerrainObject getRandomObject() { 
+        int randomValue = Random.Range(0, terrainList.Length);
+        return terrainList[randomValue];
+    }
+}
+[System.Serializable] public class TerrainObject {
+    public GameObject objectPrefab;
+    public bool surfaceSpawn = false; 
+}
--- OceanExplorers/Assets/Scripts/Terrain/TerrainGenerator.cs
+++ OceanExplorers/Assets/Scripts/Terrain/TerrainGenerator.cs
@@ -14,6 +14,8 @@ public class TerrainGenerator : MonoBehaviour {
 	public MeshSettings meshSettings;
 	public HeightMapSettings heightMapSettings;
 	public TextureData textureSettings;
+	public TerrainObjectData terrainObjectData;
+	public PossonData possonData;
 
 	public Transform viewer;
 	public Material mapMaterial;
@@ -71,7 +73,7 @@ public class TerrainGenerator : MonoBehaviour {
 					if (terrainChunkDictionary.ContainsKey(viewedChunkCoord)) {
 						terrainChunkDictionary[viewedChunkCoord].UpdateTerrainChunk();
 					} else {
-						TerrainChunk newChunk = new TerrainChunk(viewedChunkCoord, heightMapSettings, meshSettings, detailLevels, colliderLODIndex, transform, viewer, mapMaterial);
+						TerrainChunk newChunk = new TerrainChunk(viewedChunkCoord, heightMapSettings, meshSettings, detailLevels, colliderLODIndex, transform, viewer, mapMaterial, terrainObjectData, possonData);
 						terrainChunkDictionary.Add(viewedChunkCoord, newChunk);
 						newChunk.onVisibilityChanged += OnTerrainChunkVisibilityChanged;
 						newChunk.Load();
