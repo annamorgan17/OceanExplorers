9894c1c9be8d103fed13ccf62e3be1ae7eeb3999
PhoebePudge <55756132+PhoebePudge@users.noreply.github.com>
2021-04-09 19:43:29 +0100

    Lilliania - VR scripts upload

---
+++ OceanExplorers/Assets/Scripts/VR Scripts/TeleportMoveVR.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VR;
+
+public class TeleportMoveVR : MonoBehaviour
+{
+    private string joystickName;
+    private string[] joystick;
+
+    private void Start()
+    {
+        joystick = UnityEngine.Input.GetJoystickNames();
+        Debug.Log(joystick);
+    }
+
+    private void Update()
+    {
+        for(int i = 0; i < joystick.Length; i++)
+        {
+            joystickName = joystick[i];
+            Debug.Log(joystickName);
+        }
+
+        UnityEngine.Input.IsJoystickPreconfigured(joystickName);
+    }
+}
