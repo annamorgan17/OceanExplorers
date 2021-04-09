9e677156c90fea82daf32aac2b1d15a593bca485
PhoebePudge <55756132+PhoebePudge@users.noreply.github.com>
2021-04-09 19:13:23 +0100

    Phoebe - Button VR Script

---
+++ OceanExplorers/Assets/Scripts/ButtonTrigger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using Phi.ButtonExstensions;
+[RequireComponent(typeof(Collider))]
+public class ButtonTrigger : MonoBehaviour
+{
+    [SerializeField]
+    private UnityEvent onButtonPressed;
+    private bool pressedInProgress = false;
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.isTriggerButton() && !pressedInProgress) {
+            pressedInProgress = true;
+            onButtonPressed?.Invoke();
+        }
+    }
+    private void OnTriggerExit(Collider other) {
+        if (other.isTriggerButton()) {
+            pressedInProgress = false;
+            onButtonPressed?.Invoke();
+        }
+    }
+}
