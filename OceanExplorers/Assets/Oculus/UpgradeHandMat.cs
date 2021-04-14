2e5a0321d56b3acf2ab89135c15d9fdb43909e45
PhoebePudge <55756132+PhoebePudge@users.noreply.github.com>
2021-04-15 00:33:38 +0100

    Phoebe - VR Teleportation, New test scene, VR Button detection

---
+++ OceanExplorers/Assets/Oculus/UpgradeHandMat.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradeHandMat : MonoBehaviour {
+    //Quick and dirty was to set controllers to upgraded materials
+    //Found in the Unity forums
+
+
+    public Material mat; // this is the mat you want the hands changed to.
+
+
+    private GameObject handLeft;
+    private GameObject handRight;
+
+    //
+    void Update() {
+        // find and grab the hand objects
+        handRight = GameObject.Find("hand_right_renderPart_0");
+        handLeft = GameObject.Find("hand_left_renderPart_0");
+
+        // if i've found the hands change the texture
+        if (handRight != null && handLeft != null) {
+            handLeft.GetComponent<Renderer>().material = mat;
+            handRight.GetComponent<Renderer>().material = mat;
+            Debug.LogError("kkkkkkkkk");
+            Destroy(GetComponent<UpgradeHandMat>()); // remove this script so it stops running.
+        }
+    }
+}
---
+++ OceanExplorers/Assets/Scripts/GetInput.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using OVR;
+
+public class GetInput : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (OVRInput.GetDown(OVRInput.Button.Back)) { // b button
+            Debug.Log("Throw some countdown to return to main menu");
+            //if on menu exit game
+        }
+        if (OVRInput.Get(OVRInput.RawButton.LIndexTrigger)) { // right side button
+            Debug.Log("Scan a fish");
+        }
+        if (OVRInput.GetDown(OVRInput.RawButton.X)) { // z button
+            Debug.Log("Show Option menu");
+        }
+        if (OVRInput.GetDown(OVRInput.RawButton.Y)) { // z button
+            Debug.Log("Show Option menu");
+        }
+    }
+}
